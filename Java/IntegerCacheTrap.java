1dba43be5b9bc6b20ce932a4b7e0c695a131a03b
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-01-22 19:14:49 +0530

    Add IntegerCacheTrap class to demonstrate caching behavior

---
+++ Java/IntegerCacheTrap.java
@@ -0,0 +1,17 @@
+public class IntegerCacheTrap {
+
+    public static void main(String[] args) {
+
+        Integer a = 127;
+        Integer b = 127;
+
+        Integer c = 128;
+        Integer d = 128;
+
+        System.out.println(a == b);
+        System.out.println(c == d);
+
+        System.out.println(a.equals(b));
+        System.out.println(c.equals(d));
+    }
+}
