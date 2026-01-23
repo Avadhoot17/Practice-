505e8f2ef01715ed334f474a8f73c411fddbedaa
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-01-23 20:09:27 +0530

    Create IntDouble.java

---
+++ Java/IntDouble.java
@@ -0,0 +1,8 @@
+class CalcTrap2 {
+    public static void main(String[] args) {
+        double x = 10 / 4;
+        double y = 10 / 4.0;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
