025f95d777ad87bd0b12600e2643762d43488662
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-01-23 20:12:30 +0530

    Create BitwiseCalculator.java

---
+++ Java/BitwiseCalculator.java
@@ -0,0 +1,18 @@
+public class BitwiseCalculator {
+
+    public static void main(String[] args) {
+
+        int a = 12;
+        int b = 5;
+
+        int r1 = a & b + 2;
+        int r2 = a | b << 1;
+        int r3 = a ^ b >> 1;
+        int r4 = ~a + b;
+
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println(r3);
+        System.out.println(r4);
+    }
+}
