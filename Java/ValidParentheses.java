8263190c5a6ee3ef28abb8de5e06ca03fb76f279
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-02-03 09:40:41 +0530

    Create ValidParentheses.java

---
+++ Java/ValidParentheses.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[')
+                stack.push(c);
+            else {
+                if (stack.isEmpty()) return false;
+                char open = stack.pop();
+                if ((c == ')' && open != '(') ||
+                    (c == '}' && open != '{') ||
+                    (c == ']' && open != '['))
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("{[()]}"));
+    }
+}
