a476a84e00197422e10e15a01453b5e7af439d46
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-02-06 15:32:33 +0530

    Create BestTimeStock.java

---
+++ Java/BestTimeStock.java
@@ -0,0 +1,19 @@
+public class BestTimeStock {
+public static int maxProfit(int[] prices) {
+int min = Integer.MAX_VALUE;
+int profit = 0;
+
+
+for (int p : prices) {
+if (p < min) min = p;
+else profit = Math.max(profit, p - min);
+}
+return profit;
+}
+
+
+public static void main(String[] args) {
+int[] prices = {7, 1, 5, 3, 6, 4};
+System.out.println(maxProfit(prices));
+}
+}
