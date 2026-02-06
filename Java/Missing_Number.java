968d299942a984e266c031c7d2349b2f3a353010
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-02-06 15:34:41 +0530

    Create Missing_Number.java

---
+++ Java/Missing_Number.java
@@ -0,0 +1,18 @@
+public class MissingNumber {
+public static int missingNumber(int[] nums) {
+int n = nums.length;
+int sum = n * (n + 1) / 2;
+
+
+for (int num : nums) sum -= num;
+
+
+return sum;
+}
+
+
+public static void main(String[] args) {
+int[] arr = {3, 0, 1};
+System.out.println(missingNumber(arr));
+}
+}
