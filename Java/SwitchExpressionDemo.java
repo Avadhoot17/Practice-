12922f8a76ab3f91ddac4903467852556412b7ee
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-02-16 18:57:10 +0530

    Add SwitchExpressionDemo to demonstrate switch expression

---
+++ Java/SwitchExpressionDemo.java
@@ -0,0 +1,14 @@
+public class SwitchExpressionDemo {
+    public static void main(String[] args) {
+
+        int day = 3;
+
+        String result = switch (day) {
+            case 1, 2, 3, 4, 5 -> "Weekday";
+            case 6, 7 -> "Weekend";
+            default -> "Invalid";
+        };
+
+        System.out.println(result);
+    }
+}
