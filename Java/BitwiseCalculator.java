58f20c8ec63ac0793ec793d6074591c45a2ae61f
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-02-20 17:33:23 +0530

    Update BitwiseCalculator.java

--- Java/BitwiseCalculator.java
+++ Java/BitwiseCalculator.java
@@ -1,18 +1,46 @@
-public class BitwiseCalculator {
+import java.util.Scanner;
+
+public class AdvancedBitwiseCalculator {
 
     public static void main(String[] args) {
 
-        int a = 10;
-        int b = 4;
+        Scanner sc = new Scanner(System.in);
 
-        int r1 = a & b + 2;
-        int r2 = a | b << 1;
-        int r3 = a ^ b >> 1;
-        int r4 = ~a + b;
+        System.out.print("Enter first number (a): ");
+        int a = sc.nextInt();
 
-        System.out.println(r1);
-        System.out.println(r2);
-        System.out.println(r3);
-        System.out.println(r4);
+        System.out.print("Enter second number (b): ");
+        int b = sc.nextInt();
+
+        System.out.println("\nBinary Representation:");
+        System.out.println("a = " + a + " -> " + toBinary(a));
+        System.out.println("b = " + b + " -> " + toBinary(b));
+
+        // Understanding operator precedence
+        int r1 = a & (b + 2);
+        int r2 = a | (b << 1);
+        int r3 = a ^ (b >> 1);
+        int r4 = (~a) + b;
+
+        System.out.println("\nResults:");
+        printResult("a & (b + 2)", r1);
+        printResult("a | (b << 1)", r2);
+        printResult("a ^ (b >> 1)", r3);
+        printResult("(~a) + b", r4);
+
+        sc.close();
+    }
+
+    // Convert number to 32-bit binary format
+    private static String toBinary(int num) {
+        return String.format("%32s", Integer.toBinaryString(num))
+                .replace(' ', '0');
+    }
+
+    private static void printResult(String expression, int result) {
+        System.out.println("----------------------------------------");
+        System.out.println(expression);
+        System.out.println("Decimal: " + result);
+        System.out.println("Binary : " + toBinary(result));
     }
 }
