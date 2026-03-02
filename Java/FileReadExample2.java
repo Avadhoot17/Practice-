9e447007a863331a18fbce69ac1fd44e79c5c8ee
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-03-03 01:29:17 +0530

    Create FileReadExample2.java

---
+++ Java/FileReadExample2.java
@@ -0,0 +1,24 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriteExample2 {
+
+    public static void main(String[] args) {
+
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("output.txt"));
+
+            bw.write("Hello Java");
+            bw.newLine();
+            bw.write("BufferedWriter Example");
+
+            bw.close();
+
+            System.out.println("File created successfully.");
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
