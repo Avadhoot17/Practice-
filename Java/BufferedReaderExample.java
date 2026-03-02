1633b34aa437ccf0d05b49802eef5166e882b8ac
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-03-03 01:25:11 +0530

    Create BufferedReaderExample.java

---
+++ Java/BufferedReaderExample.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class BufferedReaderExample {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter your name: ");
+        String name = br.readLine();
+
+        System.out.println("Hello, " + name);
+    }
+}
