3b53b190e891c87f27e22934db25cec09a1df4d5
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-03-06 01:10:55 +0530

    Update Hollowpattern.c
    User can choose the diamond size
    • Pattern logic moved to a function
    • Option to print solid or hollow diamond
    • Cleaner loop structure
    • Better formatting

--- C Codes/Hollowpattern.c
+++ C Codes/Hollowpattern.c
@@ -1,31 +1,71 @@
 #include <stdio.h>
 
-int main() {
-    int i, j, n = 3;
+// Function to print diamond
+void printDiamond(int n, int type) {
+    int i, j;
 
+    // Upper part
     for (i = 0; i <= n; i++) {
+
         for (j = 0; j < n - i; j++)
             printf(" ");
+
         for (j = 0; j <= 2 * i; j++) {
-            if (j == 0 || j == 2 * i)
+
+            if (type == 1) // Hollow
+            {
+                if (j == 0 || j == 2 * i)
+                    printf("*");
+                else
+                    printf(" ");
+            }
+            else // Solid
                 printf("*");
-            else
-                printf(" ");
         }
+
         printf("\n");
     }
 
+    // Lower part
     for (i = n - 1; i >= 0; i--) {
+
         for (j = 0; j < n - i; j++)
             printf(" ");
+
         for (j = 0; j <= 2 * i; j++) {
-            if (j == 0 || j == 2 * i)
-                printf("*");
+
+            if (type == 1)
+            {
+                if (j == 0 || j == 2 * i)
+                    printf("*");
+                else
+                    printf(" ");
+            }
             else
-                printf(" ");
+                printf("*");
         }
+
         printf("\n");
     }
+}
+
+int main() {
+
+    int size, choice;
+
+    printf("Diamond Pattern Generator\n");
+    printf("-------------------------\n");
+
+    printf("Enter diamond size: ");
+    scanf("%d", &size);
+
+    printf("\nChoose type\n");
+    printf("1. Hollow Diamond\n");
+    printf("2. Solid Diamond\n");
+    printf("Enter choice: ");
+    scanf("%d", &choice);
+
+    printDiamond(size, choice);
 
     return 0;
 }
