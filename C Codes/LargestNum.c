0f714713045df2ca8e0cccc5a1a31c9ab0e7e010
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-03-06 01:12:08 +0530

    Update LargestNum.c
    User enters array size
    • User inputs array elements
    • Finds maximum and minimum
    • Calculates sum and average
    • Uses functions for cleaner structure
    • Automatically calculates array length
    
    Enter number of elements: 5
    Enter 5 elements:
    4 7 1 9 3
    
    Array Analysis
    -----------------
    Maximum : 9
    Minimum : 1
    Sum     : 24
    Average : 4.80

--- C Codes/LargestNum.c
+++ C Codes/LargestNum.c
@@ -1,14 +1,61 @@
 #include <stdio.h>
 
-int main() {
-    int arr[] = {4, 1, 9, 3};
+int findMax(int arr[], int n) {
     int max = arr[0];
 
-    for (int i = 1; i < 4; i++) {
+    for (int i = 1; i < n; i++) {
         if (arr[i] > max)
             max = arr[i];
     }
 
-    printf("Max: %d", max);
+    return max;
+}
+
+int findMin(int arr[], int n) {
+    int min = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < min)
+            min = arr[i];
+    }
+
+    return min;
+}
+
+int sumArray(int arr[], int n) {
+    int sum = 0;
+
+    for (int i = 0; i < n; i++)
+        sum += arr[i];
+
+    return sum;
+}
+
+int main() {
+
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int max = findMax(arr, n);
+    int min = findMin(arr, n);
+    int sum = sumArray(arr, n);
+    float avg = (float)sum / n;
+
+    printf("\nArray Analysis\n");
+    printf("-----------------\n");
+    printf("Maximum : %d\n", max);
+    printf("Minimum : %d\n", min);
+    printf("Sum     : %d\n", sum);
+    printf("Average : %.2f\n", avg);
+
     return 0;
 }
