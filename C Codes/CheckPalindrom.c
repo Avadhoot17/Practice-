8b8a7a3d39f19ad1015e56ed6702254cea7f71b9
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-03-06 01:07:52 +0530

    Update CheckPalindrom.c
    User can enter any number
    • Handles negative numbers
    • Shows the reversed number
    • Allows checking multiple numbers
    • Cleaner structure with a function

--- C Codes/CheckPalindrom.c
+++ C Codes/CheckPalindrom.c
@@ -1,17 +1,55 @@
 #include <stdio.h>
 
-int main() {
-    int n = 121, temp = n, rev = 0;
+// Function to check palindrome
+int isPalindrome(int num) {
+    int temp = num, rev = 0;
 
-    while (n > 0) {
-        rev = rev * 10 + n % 10;
-        n /= 10;
+    while (temp != 0) {
+        rev = rev * 10 + temp % 10;
+        temp /= 10;
     }
 
-    if (temp == rev)
-        printf("It's a Palindrome");
-    else
-        printf("It's Not a Palindrome");
+    return (rev == num);
+}
+
+int reverseNumber(int num) {
+    int rev = 0;
+
+    while (num != 0) {
+        rev = rev * 10 + num % 10;
+        num /= 10;
+    }
+
+    return rev;
+}
+
+int main() {
+    int num, choice;
+
+    do {
+        printf("\nEnter a number: ");
+        scanf("%d", &num);
+
+        if (num < 0) {
+            printf("Negative numbers are not considered palindrome.\n");
+        } else {
+            int reversed = reverseNumber(num);
+
+            printf("Original Number : %d\n", num);
+            printf("Reversed Number : %d\n", reversed);
+
+            if (isPalindrome(num))
+                printf("Result : It is a Palindrome\n");
+            else
+                printf("Result : Not a Palindrome\n");
+        }
+
+        printf("\nCheck another number? (1 = Yes, 0 = No): ");
+        scanf("%d", &choice);
+
+    } while (choice == 1);
+
+    printf("\nProgram Ended.\n");
 
     return 0;
 }
