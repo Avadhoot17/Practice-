9d5bc883a4ccba47c8885f37aaf0a76fac7108e4
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-03-10 02:22:19 +0530

    Update FibonacciSeries.java

--- Java/FibonacciSeries.java
+++ Java/FibonacciSeries.java
@@ -4,12 +4,12 @@ class FibonacciSeries {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter number of terms: ");
+        System.out.print("Enter number of terms:  ");
         int n = sc.nextInt();
 
         int a = 0, b = 1;
 
-        System.out.print("Fibonacci Series: ");
+        System.out.print("Fibonacci Series:  ");
 
         for (int i = 1; i <= n; i++) {
             System.out.print(a + " ");
