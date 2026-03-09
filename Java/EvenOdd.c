ebc56d021a38999c2018d3014a3947c796f7c5bd
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-03-10 02:16:49 +0530

    Update EvenOdd.c

--- Java/EvenOdd.c
+++ Java/EvenOdd.c
@@ -1,12 +1,12 @@
-// Program to check whether a number is even or odd
+// Program to check whether a number is even or odd 
 #include <stdio.h>
 
 int main() {
 
-    // Number to be checked
+    // Number to be checked 
     int number = 7;
 
-    // Check even or odd using modulus operator
+    // Check even or odd using modulus operator 
     if (number % 2 == 0) {
         printf("Even Number\n");
     } else {
