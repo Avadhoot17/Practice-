0bce19da0f1b2385873d5b075e7a60c6b548bdfe
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-03-13 00:19:07 +0530

    Create CreateLinkedList.java

---
+++ Java/CreateLinkedList.java
@@ -0,0 +1,29 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class CreateLinkedList {
+
+    public static void display(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+
+        display(head);
+    }
+}
