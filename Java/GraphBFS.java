b30564d2ba22a09d603753b4e488f1e5f15fd90f
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-03-14 02:45:40 +0530

    Create GraphBFS.java

---
+++ Java/GraphBFS.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class GraphBFS {
+
+    private int V;
+    private LinkedList<Integer>[] adj;
+
+    GraphBFS(int v) {
+        V = v;
+        adj = new LinkedList[v];
+
+        for (int i = 0; i < v; i++)
+            adj[i] = new LinkedList<>();
+    }
+
+    void addEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
+    void BFS(int s) {
+
+        boolean[] visited = new boolean[V];
+        LinkedList<Integer> queue = new LinkedList<>();
+
+        visited[s] = true;
+        queue.add(s);
+
+        while (!queue.isEmpty()) {
+            s = queue.poll();
+            System.out.print(s + " ");
+
+            for (int n : adj[s]) {
+                if (!visited[n]) {
+                    visited[n] = true;
+                    queue.add(n);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        GraphBFS g = new GraphBFS(4);
+
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 3);
+        g.addEdge(2, 3);
+
+        g.BFS(0);
+    }
+}
