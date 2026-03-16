8a770aa02be3cf66cc895d79bac930684df6564b
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-03-16 18:52:57 +0530

    Create HelloServlet.java

---
+++ Java/HelloServlet.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println("<html><body>");
+        out.println("<h1>Hello, Welcome to Java Servlets!</h1>");
+        out.println("</body></html>");
+    }
+}
