064fab7a59b7603290839d007f8591c844c1c2fb
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-03-16 18:54:41 +0530

    Create CookieServlet.java

---
+++ Java/CookieServlet.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class CookieServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        Cookie cookie = new Cookie("user", "Avadhoot");
+        response.addCookie(cookie);
+
+        out.println("<h2>Cookie Created Successfully</h2>");
+    }
+}
