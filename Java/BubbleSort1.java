481835507cc4fc67b7a51bc8d0e430d2bf167ea7
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-03-17 01:58:30 +0530

    Create BubbleSort1.java

---
+++ Java/BubbleSort1.java
@@ -0,0 +1,23 @@
+public class BubbleSort1 {
+
+    public static void main(String[] args) {
+
+        int arr[] = {5,2,9,1,6};
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+
+                if(arr[j] > arr[j+1]){
+
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+
+                }
+            }
+        }
+
+        for(int i: arr)
+            System.out.print(i+" ");
+    }
+}
