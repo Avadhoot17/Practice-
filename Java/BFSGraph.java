8ae623159bd59b53d75926f8cb815109a0c37b37
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-03-17 01:59:40 +0530

    Create BFSGraph.java

---
+++ Java/BFSGraph.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class BFSGraph {
+
+    public static void main(String[] args) {
+
+        int V = 5;
+
+        List<Integer> adj[] = new ArrayList[V];
+
+        for(int i=0;i<V;i++)
+            adj[i] = new ArrayList<>();
+
+        adj[0].add(1);
+        adj[0].add(2);
+        adj[1].add(3);
+        adj[2].add(4);
+
+        boolean visited[] = new boolean[V];
+
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(0);
+        visited[0] = true;
+
+        while(!q.isEmpty()){
+
+            int node = q.poll();
+            System.out.print(node + " ");
+
+            for(int neighbor : adj[node]){
+
+                if(!visited[neighbor]){
+                    visited[neighbor] = true;
+                    q.add(neighbor);
+                }
+            }
+        }
+    }
+}
