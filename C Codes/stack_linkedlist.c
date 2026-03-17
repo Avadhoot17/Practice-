34591b87dd2b7fd0fce59f9245d63db3d1e8888a
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-03-18 01:52:35 +0530

    Create stack_linkedlist.c

---
+++ C Codes/stack_linkedlist.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* top = NULL;
+
+void push(int x) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = x;
+    newNode->next = top;
+    top = newNode;
+}
+
+void pop() {
+    if (top == NULL) {
+        printf("Stack Underflow\n");
+    } else {
+        struct Node* temp = top;
+        printf("Popped: %d\n", temp->data);
+        top = top->next;
+        free(temp);
+    }
+}
+
+void display() {
+    struct Node* temp = top;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    push(5); push(15); push(25);
+    display();
+    pop();
+    display();
+    return 0;
+}
