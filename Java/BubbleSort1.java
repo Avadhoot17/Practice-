9783c02df03f3650f878e026fec54b176756449a
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-03-23 23:34:45 +0530

    Update BubbleSort1.java

--- Java/BubbleSort1.java
+++ Java/BubbleSort1.java
@@ -2,7 +2,7 @@ public class BubbleSort1 {
 
     public static void main(String[] args) {
 
-        int arr[] = {5,2,9,1,6};
+        int arr[] = {4,2,9,1,6};
 
         for(int i=0;i<arr.length;i++){
             for(int j=0;j<arr.length-i-1;j++){
