9bec7b71eac7509aab0d2cdc5e95e735a49e51e5
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-03-30 23:20:25 +0530

    Update AutoRunMethods.java

--- Java/AutoRunMethods.java
+++ Java/AutoRunMethods.java
@@ -1,7 +1,7 @@
 import java.lang.annotation.*;
 import java.lang.reflect.*;
 
-// ---------- Custom Annotation ----------aaaaaaaa
+// ---------- Custom Annotation ----------aaaaaaaaa
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 @interface AutoRun {
