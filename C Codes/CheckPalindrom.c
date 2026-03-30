b8bf3b21c168ac7f0bf478c9c3f515af3aaedf5a
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-03-31 01:28:13 +0530

    Fix comment formatting in CheckPalindrom.c

--- C Codes/CheckPalindrom.c
+++ C Codes/CheckPalindrom.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// Function to check palindrome  12345678910111213141516
+// Function to check palindrome  1234567891011121314151617
 int isPalindrome(int num) {
     int temp = num, rev = 0;
 
