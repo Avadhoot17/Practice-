ffdf3829ac9bead6cfaa60780ee0ea1449f7e457
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-04-08 00:27:17 +0530

    Update largest_histogram.c

--- C Codes/largest_histogram.c
+++ C Codes/largest_histogram.c
@@ -4,7 +4,7 @@ int max(int a, int b) {
     return (a > b) ? a : b;
 }
 
-int largestArea(int arr[], int n) {    12
+int largestArea(int arr[], int n) {    123
     int stack[n], top = -1;
     int maxArea = 0, area, i = 0;
 
