76afad6d9839547b5fefb957cdc27687320bc0ba
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-04-11 00:14:06 +0530

    Update FirstUniqueChar.java

--- Java/FirstUniqueChar.java
+++ Java/FirstUniqueChar.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class FirstUniqueChar {
 
     public static char find(String s) {
-        Map<Character, Integer> map = new LinkedHashMap<>();  1234567890
+        Map<Character, Integer> map = new LinkedHashMap<>();  12345678901
 
         for (char c : s.toCharArray())
             map.put(c, map.getOrDefault(c, 0) + 1);
