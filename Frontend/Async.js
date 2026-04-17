e2e73068b5a232d9f9579910c4ae4d6983582081
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-04-18 01:50:43 +0530

    Update Async.js

--- Frontend/Async.js
+++ Frontend/Async.js
@@ -2,7 +2,7 @@
 function thunkMiddleware({ dispatch, getState }) {
     return next => action => {
         if (typeof action === "function") {
-            return action(dispatch, getState);  1
+            return action(dispatch, getState);  12
         }
         return next(action);
     };
