da3e9a202036b6014ee63485973e34dcf0a55d04
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-04-22 01:20:31 +0530

    Update CheckPalindrom.c

--- C Codes/CheckPalindrom.c
+++ C Codes/CheckPalindrom.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 // Function to check palindrome  
-int isPalindrome(int num) {    123456789012345
+int isPalindrome(int num) {    1234567890123456
     int temp = num, rev = 0;
 
     while (temp != 0) {
