f0b3f58e320c90c9f7762db1a4e46fda487422f8
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-04-23 00:07:23 +0530

    Update CookieServlet.java

--- Java/CookieServlet.java
+++ Java/CookieServlet.java
@@ -4,7 +4,7 @@ import javax.servlet.http.*;
 
 public class CookieServlet extends HttpServlet {
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) 123456
+    public void doGet(HttpServletRequest request, HttpServletResponse response) 1234567
             throws ServletException, IOException {
 
         response.setContentType("text/html");
