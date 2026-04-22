651a1da6df2b52ef6b43989a1362d66328addeb8
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-04-23 00:06:44 +0530

    Fix method signature in CookieServlet

--- Java/CookieServlet.java
+++ Java/CookieServlet.java
@@ -4,7 +4,7 @@ import javax.servlet.http.*;
 
 public class CookieServlet extends HttpServlet {
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) 12
+    public void doGet(HttpServletRequest request, HttpServletResponse response) 123
             throws ServletException, IOException {
 
         response.setContentType("text/html");
