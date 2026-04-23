949b816730bc3bd0bbf3a493afa133326e204fb4
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-04-24 02:13:04 +0530

    Update BankAccountApp.java

--- Java/BankAccountApp.java
+++ Java/BankAccountApp.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class BankAccount {
 
-    private String accountHolder; 12345678901
+    private String accountHolder; 123456789012
     
     private double balance;
 
