ae424e9390ffcbe81c28b10520965c5ce61a261e
Avadhoot Welde <157208745+Avadhoot17@users.noreply.github.com>
2026-05-18 21:20:29 +0530

    Update CountVouvles.c

--- C Codes/CountVouvles.c
+++ C Codes/CountVouvles.c
@@ -2,7 +2,7 @@
 #include <ctype.h>
 
 void analyzeString(char str[]) {
-    int vowels = 0, consonants = 0, digits = 0, spaces = 0; 12
+    int vowels = 0, consonants = 0, digits = 0, spaces = 0; 123
 
     for (int i = 0; str[i] != '\0'; i++) {
         char c = str[i];
